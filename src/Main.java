369be58914f9136b86bc03371d09dfcd4f75d7b0
ftlab <diegogranada7@gmail.com>
2018-01-30 11:49:33 -0500

    * Changed from arraylist to stack * made the basic method for all calculations * deleted factorial() *

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
     static char[] symbols = new char[]{'+','-','+','/','^','|','v','~','s','c','t'};
@@ -32,24 +33,30 @@ public class Main {
             if(input.equalsIgnoreCase("quit")){
                 bContinue = false;
             }else{
-                //Debug, change
-                //System.out.println("Result is"+factorial(Integer.parseInt(input)));
+                Stack<String> terms = new Stack<String>();
                 char[] chars = input.toCharArray();
-                ArrayList<String> terms = new ArrayList<String>();
+                //ArrayList<String> terms = new ArrayList<String>();
                 StringBuilder currentTerm = new StringBuilder();
                 for (int i = 0; i < chars.length; i++) {
 
-                    if(Character.isDigit(chars[i])){
+                    if (Character.isDigit(chars[i])) {
                         currentTerm.append(chars[i]);
-                    }else if(chars[i]==' '){
-                        terms.add(currentTerm.toString());
+                    } else if (chars[i] == ' ') {
+                        terms.push(currentTerm.toString());
                         //Check this, not sure if it will empty the StringBuilder correctly.
                         currentTerm = new StringBuilder();
-
-                    }else if(isSymbol(chars[i])){
-
+                    }else if (isSymbol(chars[i])){
+                        if(Arrays.asList(symbols).indexOf(chars[i])<=4){
+                            terms.push(String.valueOf(calculate(chars[i], Double.parseDouble(terms.pop()),Double.parseDouble(terms.pop()))));
+                        }else{
+                            terms.push(String.valueOf(calculate(chars[i], Double.parseDouble(terms.pop()))));
+                        }
                     }
 
+                }
+                terms.push(currentTerm.toString());
+                for(String a: terms){
+                    System.out.println(a+" ");
                 }
             }
         }
@@ -62,14 +69,54 @@ public class Main {
         }
         return false;
     }
-
-    private static int factorial(int input){
-        int prox = 1;
-        for (int i = 1; i < input+1; i++) {
-            prox = prox * i;
-            System.out.println("DEBUG prox = "+prox + ", i = "+i);
-        }
-        return prox;
+    //'+','-','+','/','^','|','v','~','s','c','t'
+    private static double calculate(char operator, double op1, double op2) {
+        if (operator == symbols[0]) {  //+
+            return op1 + op2;
+        } else if (operator == symbols[1]) {  // -
+            return op2 - op1;
+        } else if (operator == symbols[2]) {  // *
+            return op2 * op1;
+        } else if (operator == symbols[3]) {  // /
+            return op2 / op1;
+        } else if (operator == symbols[4]) {  // ^
+            return Math.pow(op2, op1);
+        } return 0;
     }
+    private static double calculate(char operator, double op1){
+        if (operator == symbols[5]) {  // |
+            return Math.abs(op1);
+        } else if (operator == symbols[6]) {  // v
+            return Math.sqrt(op1);
+        } else if (operator == symbols[7]) {  // ~
+            return Math.round(op1);
+        } else if (operator == symbols[8]) {  // s
+            return Math.sin(op1);
+        } else if (operator == symbols[9]) {  // c
+            return Math.cos(op1);
+        } else if (operator == symbols[10]) {  // t
+            return Math.tan(op1);
+        } return 0;
+    }
+        /**
+         * Two-operand:
+         * Addition (+),
+         * Subtraction (-),
+         * Multiplication (*),
+         * Division (/), and
+         * Exponentiation (^)
+         *
+         One-operand:
+         Absolute Value (|),
+         Square Root (v),
+         Round to closest integer (~),
+         Sine (s),
+         Cosine (c), and
+         Tangent (t)
+
+         */
+    /*private static double process(ArrayList<String> terms){
+
+    }*/
 
 }
